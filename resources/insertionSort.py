787e750b5bba7212e8559ae05e742f248b4a068a
Benji <67171031+liangbinjie@users.noreply.github.com>
2023-05-09 11:14:48 -0600

    insertionSort

---
+++ resources/insertionSort.py
@@ -0,0 +1,10 @@
+def insertionSort(lista):
+    for i in range (1,len(lista)):
+        v=lista[i]
+        j=i-1
+        while j>=0 and lista[j]>v:
+            lista[j+1]=lista[j]
+            j=j-1
+        lista[j+1]=v
+    #print(lista)
+    return lista
