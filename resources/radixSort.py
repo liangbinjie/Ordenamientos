e6888bac9ca1911b7362c2639346e0640e983f75
Benji <67171031+liangbinjie@users.noreply.github.com>
2023-05-09 12:03:51 -0600

    RadixSort

---
+++ resources/radixSort.py
@@ -0,0 +1,61 @@
+def mayor(lista):
+    mayor = lista[0]
+    for num in lista:
+        if num > mayor:
+            mayor = num
+    return mayor
+
+def largo(num):
+    if num == 0:
+        return 1
+    i = 0
+    while num != 0:
+        i += 1
+        num = num//10
+    return i
+
+# **********************************************************
+def radixSort(lista):
+    if type(lista) == list:
+        maxValor = mayor(lista)
+        largoMax = largo(maxValor)
+        i = 0
+        while i < largoMax:
+            l0 = []
+            l1 = []
+            l2 = []
+            l3 = []
+            l4 = []
+            l5 = []
+            l6 = []
+            l7 = []
+            l8 = []
+            l9 = []
+            for num in lista:
+                temp = num//10**i%10
+                if temp == 0:
+                    l0 += [num]
+                elif temp == 1:
+                    l1 += [num]
+                elif temp == 2:
+                    l2 += [num]
+                elif temp == 3:
+                    l3 += [num]
+                elif temp == 4:
+                    l4 += [num]
+                elif temp == 5:
+                    l5 += [num]
+                elif temp == 6:
+                    l6 += [num]
+                elif temp == 7:
+                    l7 += [num]
+                elif temp == 8:
+                    l8 += [num]
+                elif temp == 9:
+                    l9 += [num]
+            lista = l0 + l1 + l2 + l3 + l4 + l5+l6+l7+l8+l9
+            i+=1
+            #print(lista)
+
+        #print(lista) 
+        return lista
